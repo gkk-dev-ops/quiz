de9a68124314dd2fb4762cf9fa0fda90e44c9a11
gkk <gkk.dev.ops@gmail.com>
2022-12-03 20:14:53 +0100

    Answers and one time selection of answers

--- src/components/QuizQuestions.js
+++ src/components/QuizQuestions.js
@@ -5,6 +5,7 @@ export default function QuizQuestion(props) {
     const [answerAClass, setAnswerAClass] = useState('');
     const [answerBClass, setAnswerBClass] = useState('');
     const [answerCClass, setAnswerCClass] = useState('');
+    let alreadyAnswered = false;
 
     function resetQuestionState() {
         setAnswerAClass('')
@@ -14,59 +15,60 @@ export default function QuizQuestion(props) {
 
     return(
         <div className="quiz-question">
-            <div className="question-tile">
-                <h1>
-                    {props.questionBase[props.questionIndex].q}
-                </h1>
-            </div>
-            <div
-                className={`answer-tile ${answerAClass}`}
-                onClick={() => {
-                    if (answerAClass === '') {                        
-                        resetQuestionState()
-                        if(props.questionBase[props.questionIndex].correct === 'A' && answerAClass === ''){
-                            setAnswerAClass("correct-answer");
-                            props.pointsSetter(props.points+1);
-                        }else{
-                            setAnswerAClass("wrong-answer");
+            <div className="quiz-tiles">
+                <div className="question-tile">
+                    <h1>
+                        {props.questionBase[props.questionIndex].q}
+                    </h1>
+                </div>
+                <div
+                    className={`answer-tile ${answerAClass}`}
+                    onClick={() => {
+                        if (answerAClass === '' && answerBClass === '' && answerCClass === ''){
+                            resetQuestionState()
+                            if(props.questionBase[props.questionIndex].correct === 'A'){
+                                setAnswerAClass("correct-answer");
+                                props.pointsSetter(props.points+1);
+                            }else{
+                                setAnswerAClass("wrong-answer")
+                            }
                         }
+                    }}
+                    >
+                    {props.questionBase[props.questionIndex].A}
+                </div>
+                <div
+                    className={`answer-tile ${answerBClass}`}
+                    onClick={() => {
+                        if (answerAClass === '' && answerBClass === '' && answerCClass === ''){
+                            resetQuestionState()
+                            if(props.questionBase[props.questionIndex].correct === 'B'){
+                                setAnswerBClass("correct-answer")
+                                props.pointsSetter(props.points+1)
+                            }else{
+                                setAnswerBClass("wrong-answer")
+                            }
+                        }
+                    }}>
+                    {props.questionBase[props.questionIndex].B}
+                </div>
+                    
+                <div
+                className={`answer-tile ${answerCClass}`}
+                onClick={() => {
+                    if (answerAClass === '' && answerBClass === '' && answerCClass === ''){
+                        resetQuestionState()
+                        if(props.questionBase[props.questionIndex].correct === 'C'){
+                            setAnswerCClass("correct-answer")
+                            props.pointsSetter(props.points+1)
+                        }else{
+                            setAnswerCClass("wrong-answer")
+                        }                        
                     }
                 }}
                 >
-                {props.questionBase[props.questionIndex].A}
-            </div>
-                
-            <div
-                className={`answer-tile ${answerBClass}`}
-                onClick={() => {
-                    if (answerBClass === ''){
-                        resetQuestionState()
-                        if(props.questionBase[props.questionIndex].correct === 'B' && answerAClass === ''){
-                            setAnswerBClass("correct-answer")
-                            props.pointsSetter(props.points+1)
-                        }else{
-                            setAnswerBClass("wrong-answer")
-                        }
-                    }
-                }}>
-                {props.questionBase[props.questionIndex].B}
-            </div>
-                
-            <div
-            className={`answer-tile ${answerCClass}`}
-            onClick={() => {
-                if (answerCClass === '') {
-                    resetQuestionState()
-                    if(props.questionBase[props.questionIndex].correct === 'C'){
-                        setAnswerCClass("correct-answer")
-                        props.pointsSetter(props.points+1)
-                    }else{
-                        setAnswerCClass("wrong-answer")
-                    }                        
-                }
-            }}
-            >
-                {props.questionBase[props.questionIndex].C}
+                    {props.questionBase[props.questionIndex].C}
+                </div>
             </div>
             <div className="check-answer">
                 <button
