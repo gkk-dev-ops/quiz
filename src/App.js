0549e4006b8d2caa5af6a1eca335744f1694a3cf
gkk <gkk.dev.ops@gmail.com>
2023-01-19 23:13:21 +0100

    Remove unused code and add mock db

--- src/App.js
+++ src/App.js
@@ -3,54 +3,32 @@ import './App.css';
 import React, {useState} from 'react';
 import QuizHeader from './components/QuizHeader'
 import QuizQuestion from './components/QuizQuestions'
+
 import qb from './questions.json'
 
+const QUESTIONS_DB_URL = 'http://localhost:3000'
+
 function App() {
+
   const[state, setState]=useState(0);
   const[questionIndex, questionIndexSetter]=useState(0);
+  
   return (
     <div className="App">
-     <QuizHeader
-     currentPoints={state}
-     amountOfQuestions={qb.length}
-     questionIndex={questionIndex}
-     />
-     <QuizQuestion
-     questionBase={qb}
-     questionIndex={questionIndex}
-     questionIndexSetter={questionIndexSetter}
-     points={state}
-     pointsSetter={setState}
-     />
+      <QuizHeader
+      currentPoints={state}
+      amountOfQuestions={qb.length}
+      questionIndex={questionIndex}
+      />
+      <QuizQuestion
+      questionBase={qb}
+      questionIndex={questionIndex}
+      questionIndexSetter={questionIndexSetter}
+      points={state}
+      pointsSetter={setState}
+      />
     </div>
   );
 }
-
-
-// const qb={
-//   "questions":[
-//       {
-//         "Q":"This is a question one?",
-//         "A":"answer a: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque inventore laboriosam voluptatem sunt officiis mollitia.",
-//         "B":"answer b: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque inventore laboriosam voluptatem sunt officiis mollitia",
-//         "C":"answer c: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque inventore laboriosam voluptatem sunt officiis mollitia",
-//         "correct":"A"
-//       },
-//       {
-//         "Q":"This is a question two?",
-//         "A":"answer a: e inventore laboriosam voluptatem sunt officiis mollitia.",
-//         "B":"answer b",
-//         "C":"answer c",
-//         "correct":"B"
-//     },
-//     {
-//       "Q":"",
-//       "A":"",
-//       "B":"",
-//       "C":"",
-//       "correct":""
-//   },
-//   ]
-// }
 
 export default App;
