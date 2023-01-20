26043508ae9cf2752fc908a043aea223c70f3e0d
gkk <gkk.dev.ops@gmail.com>
2023-01-20 22:46:08 +0100

    Add fetching questions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,47 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Masthead from '../Masthead/Masthead'
 import Questions from '../Questions/Questions'
-
-import qb from '../../questions.json'
+import axios from 'axios'
 
 const QUESTIONS_DB_URL = 'http://localhost:3001'
 
 function App() {
-
+  const [questionsDB, setQuestionsDB] = useState(null)
   const[state, setState]=useState(0);
   const[questionIndex, questionIndexSetter]=useState(0);
-  
-  return (
-    <div className="App">
-      <Masthead
-      currentPoints={state}
-      amountOfQuestions={qb.length}
-      questionIndex={questionIndex}
-      />
-      <Questions
-      questionBase={qb}
-      questionIndex={questionIndex}
-      questionIndexSetter={questionIndexSetter}
-      points={state}
-      pointsSetter={setState}
-      />
-    </div>
-  );
+
+  useEffect(() => {
+    getQuestions()
+  }, [questionIndex]);
+
+  async function getQuestions(){
+    axios.get(`${QUESTIONS_DB_URL}/gunPermit`).then(res => {
+      setQuestionsDB(res.data);
+    })
+  }
+
+  if(!questionsDB){
+    return <p>Loading questions...</p>
+  }else{
+    return (
+      <div className="App">
+        <Masthead
+        currentPoints={state}
+        amountOfQuestions={questionsDB.length}
+        questionIndex={questionIndex}
+        />
+
+        <Questions
+        questionBase={questionsDB}
+        questionIndex={questionIndex}
+        questionIndexSetter={questionIndexSetter}
+        points={state}
+        pointsSetter={setState}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
